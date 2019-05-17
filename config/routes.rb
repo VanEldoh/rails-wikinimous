d362cede095e6b437f7f434c6cdd99f05902ee9f
Vanessa Eldoh <van.eldoh@gmail.com>
2019-05-17 16:57:21 +0200

    wiki

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show', as: 'article'
+  delete 'articles/:id', to: 'articles#destroy', as: 'delete'
+  post 'articles', to: 'articles#create'
+  patch 'articles/:id', to: 'articles#update'
+  get 'articles/:id/edit', to: 'articles#edit', as: 'edit'
 end
